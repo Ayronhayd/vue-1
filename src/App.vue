7a308cf6ad82a0cb348df8f22f44ed6fa9da698a
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-09 22:34:45 +0300

    3 урок

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
-<script>
+<template>
+  <h1>{{ info }}</h1>
+  <p>{{ someInfo }}</p>
 
-</script>
-
-<template></template>
+  <button type="button" @click="userData()">сообщения</button>
+</template>
 
 <style scoped>
+h1 {
+  color: green;
+  text-align: center;
+}
 
+p {
+  color: red;
+  text-align: center;
+}
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      info: 'Title',
+      someInfo: 'Some info',
+    }
+  },
+  methods: {
+    userData() {
+      this.info = 'New title'
+      this.someInfo = 'New info'
+    }
+  }
+}
+</script>
